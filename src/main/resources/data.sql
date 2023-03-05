98b49ab977764b098a4b83d174fbfc3615d6de0b
Abhishek Singh Manhas <abhisheksmanhas@gmail.com>
2023-03-05 09:36:03 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO todo (title,status) VALUES ('Learn Spring Boot',FALSE);
+INSERT INTO todo (title,status) VALUES ('Visit Grocery Store',FALSE);
+INSERT INTO todo (title,status) VALUES ('Learn AWS',FALSE);
+INSERT INTO todo (title,status) VALUES ('Swimming',FALSE);
+INSERT INTO todo (title,status) VALUES ('Yoga',FALSE);
